e63755c24de1733994411be58908c31da56d8460
Steve Kinuthia <121674168+kinuthiiia@users.noreply.github.com>
2024-02-14 06:19:38 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import localFont from "@next/font/local";
+
+const canterbury = localFont({
+  src: "../public/fonts/Canterbury.ttf",
+  display: "swap",
+});
+
+export default function Home() {
+  return (
+    <div>
+      <img src="/bg.jpg" className="object-cover relative w-full h-screen" />
+      <div className="absolute top-[50%] translate-y-[-50%] mx-12">
+        <p
+          className={`${canterbury.className} text-black text-[3.5rem] font-[] font-bold tracking-tight`}
+        >
+          Tasha,
+        </p>
+        <br />
+
+        <p className="text-slate-600 font-medium tracking-tight text-[1.2rem] space-y-4  ">
+          <span className="block">
+            Beautiful girls like you don't fall from the sky. You are special.
+          </span>
+
+          <span className="block">
+            You are from my rib, my strength and weakness, my moon in my
+            starless dark night, my LOVE.
+          </span>
+          <span className="block"> I love you.</span>
+        </p>
+        <br />
+        <p className="text-[1.2rem] font-semibold text-black">-Kinut❤️</p>
+      </div>
+    </div>
+  );
+}
